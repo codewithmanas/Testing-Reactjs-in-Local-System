34dc58110819bd98ea339e3a2a2546d1e42d46e9
Manas Ranjan Adha <118095682+codewithmanas@users.noreply.github.com>
2023-02-02 18:50:15 +0530

    like counter demo added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
+import {useState} from 'react';
 import './App.css';
 
+
+// var headingText = "inside out...";
+var color = "green"; 
+
 function App() {
+  const [likeCounter, setLikeCounter] = useState(0);
+
+  function likeClickHandler(){
+    var newLikeCounterValue = likeCounter + 1;
+    setLikeCounter(newLikeCounterValue);
+  }  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>inside out...</h1> */}
+      <h1>inside <span style={{color : color}}>outtt...</span></h1>
+      <button onClick={likeClickHandler}>Like Me!</button>{likeCounter}
     </div>
   );
 }
